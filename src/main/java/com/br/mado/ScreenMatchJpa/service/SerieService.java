e765dd991827cd8e7883e3c54013b60c78b56761
Marcus Augusto <madogap@gmail.com>
2024-06-15 19:32:23 -0300

    Implementação do método obterPorId() no SerieService

--- src/main/java/com/br/mado/ScreenMatchJpa/service/SerieService.java
+++ src/main/java/com/br/mado/ScreenMatchJpa/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,4 +34,13 @@ public class SerieService {
         return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
     }
 
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        
+            if (serie.isPresent()){
+                Serie s = serie.get();
+                return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+            }
+            return null;
+    }
 }
