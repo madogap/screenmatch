766d61b824cd9aa6b6c15cd398d674fd802f1b80
Marcus Augusto <madogap@gmail.com>
2024-06-15 18:04:20 -0300

    Criação da Classe SerieService: Criada a classe SerieService e anotada com @Service. Essa classe será responsável por implementar a regra de negócio da aplicação. Injeção do SerieRepository: Adicionada a anotação @Autowired para injetar o SerieRepository na classe SerieService. Isso permite que a classe de serviço acesse os dados do banco de dados. Implementação do Método obterTodasAsSeries(): Implementado o método obterTodasAsSeries() na classe SerieService. Esse método utiliza o método findAll() do repositório para obter todas as séries. Os resultados são mapeados para a classe SerieDTO usando o stream() e collect(Collectors.toList()). Injeção do SerieService no SerieController: Adicionada a anotação @Autowired para injetar o SerieService na classe SerieController. Isso permite que o controlador utilize o método obterTodasAsSeries() da classe de serviço. Implementação do Endpoint /series: Criado o endpoint /series no SerieController. Esse endpoint chama o método obterTodasAsSeries() da classe de serviço e retorna a lista de séries. Limpeza e Organização do Código: Removidos os imports desnecessários, mantendo o código mais limpo e legível. Seguidas as boas práticas de desenvolvimento, como baixo acoplamento e alta coesão.

--- src/main/java/com/br/mado/ScreenMatchJpa/controller/SerieController.java
+++ src/main/java/com/br/mado/ScreenMatchJpa/controller/SerieController.java
@@ -1,25 +1,22 @@
 package com.br.mado.ScreenMatchJpa.controller;
 
 import com.br.mado.ScreenMatchJpa.dto.SerieDTO;
-import com.br.mado.ScreenMatchJpa.repository.SerieRepository;
+import com.br.mado.ScreenMatchJpa.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
     @Autowired
-    private SerieRepository repository;
+    private SerieService servico;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries(){
-        return repository.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());}
-
+        return servico.obterTodasAsSeries();
+    }
 }
+
---
+++ src/main/java/com/br/mado/ScreenMatchJpa/service/SerieService.java
@@ -0,0 +1,23 @@
+package com.br.mado.ScreenMatchJpa.service;
+
+import com.br.mado.ScreenMatchJpa.dto.SerieDTO;
+import com.br.mado.ScreenMatchJpa.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries(){
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+
+    }
+}
