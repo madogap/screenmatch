dd864d94ef80de1a439ad7ef7593daf613059f92
Marcus Augusto <madogap@gmail.com>
2024-06-15 14:40:17 -0300

    Alterado o retoro do método obterSerie de String para List<Serie>, injeção do SerieRepository na classe SerieController, utilizando a anotação @Autowired. Chamado o método findAll() do repositorio para retonar todas as séries do banco de dados. Identificado um problema de referência circular, onde uma série possuii uma lista de episódios e cada episódio possui uma referência à série, esse problema causa um loop infinito na serialização dos dados para JSON. Para resolver o problema de referência circular usarei de DTOs, mas também para ocultar dados sensíveis que não devem ser entregues para usuário final.

--- src/main/java/com/br/mado/ScreenMatchJpa/controller/SerieController.java
+++ src/main/java/com/br/mado/ScreenMatchJpa/controller/SerieController.java
@@ -1,14 +1,22 @@
 package com.br.mado.ScreenMatchJpa.controller;
 
+import com.br.mado.ScreenMatchJpa.model.Serie;
+import com.br.mado.ScreenMatchJpa.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String obterSeries(){
-        return "Aqui vão ser listadas as séries!";
+    public List<Serie> obterSeries(){
+        return repository.findAll();
     }
 
 }
